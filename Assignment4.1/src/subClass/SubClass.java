2756d3616b390ccc998544eca31e8b7de6376e68
pkatira7 <pkatira@vmware.com>
2017-01-19 13:00:16 +0530

    Add files via upload

---
+++ Assignment4.1/src/subClass/SubClass.java
@@ -0,0 +1,15 @@
+package subClass;
+
+import  superClass.*;
+
+public class SubClass extends SuperClass{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	 SubClass y = new SubClass();
+	 System.out.println(y.getX());
+		
+	}
+
+}
---
+++ Assignment4.1/src/superClass/SuperClass.java
@@ -0,0 +1,16 @@
+package superClass;
+
+public class SuperClass {
+	int x = 0;
+	
+
+	
+	protected int getX() {
+		return x;
+	}
+
+	protected void setX(int x) {
+		this.x = x;
+	}
+
+}
